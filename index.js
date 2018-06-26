e15486786c74f44c25107e72a74057f506e80bcc
Elle Allen <ellealle@iu.edu>
2018-06-26 14:55:18 -0400

    now the button does something

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const button = document.querySelector('button')
+
+const changeHeader = function() {
+  const p = document.querySelector('h1#soHandsome')
+  p.textContent = 'why are there so many there arent that many matts or micheals'
+}
+
+button.addEventListener('click', changeHeader)
