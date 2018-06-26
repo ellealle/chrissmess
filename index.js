a445a4dd5027d91e0482cd117bea828605e9854e
Elle Allen <ellealle@iu.edu>
2018-06-26 15:33:50 -0400

    Trying to trigger every header to change

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const button = document.querySelector('button')
 
 const changeHeader = function() {
-  const p = document.querySelector('h1#soHandsome')
-  p.textContent = 'why are there so many there arent that many matts or micheals'
+  const p = document.querySelector('p#soMany')
+  p.textContent = 'There\'s too many Chris\''
 }
 
+const changeHeaderDos = function() {
+    const p = document.querySelector('p#Marvel')
+    p.textContent = 'Where did they all come from?'
+  }
+  const changePara = function() {
+    const p = document.querySelector('p#soHandsome')
+    p.textContent = 'Why are there so many Chris\', there aren\'t that many Matthew\'s or Micheal\'s, how did they tell them all apart for Infinity War'
+  }
+
 button.addEventListener('click', changeHeader)
+button.addEventListener('click', changeHeaderDos)
+button.addEventListener('click', changePara)
