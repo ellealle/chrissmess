237836d231edadbc70bf48103f7e89976bf31555
Elle Allen <ellealle@iu.edu>
2018-06-27 23:10:08 -0400

    day 2 homework completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const button = document.querySelector('button')
+const buttonDos = document.querySelector('buttonDos')
+const form = document.querySelector('form#flickForm')
 
 const changeHeader = function() {
   const p = document.querySelector('h1#soHandsome')
@@ -10,5 +12,20 @@ const changeHeaderDos = function() {
     p.textContent = 'Where did they all even come from'
   }
   
+const addChris = function(ev) {
+    ev.preventDefault()
+    const f = ev.target
+  
+    const chrisMovie = f.chrisMovie.value
+    const item = document.createElement('li')
+    item.textContent = chrisMovie
+  
+    const list = document.querySelector('#chrisn')
+    item.classList.add('')
+    list.appendChild(item)
+  
+    f.reset()
+  }
 button.addEventListener('click', changeHeader)
 button.addEventListener('click', changeHeaderDos)
+
